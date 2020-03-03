849d7e303b058e3fdd2646ecd89ee6c0a2e05b26
Your Name <juniorvilchezramos12@gmail.com>
2020-03-02 19:02:03 -0500

    first commit

---
+++ pjConexion2/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace pjConexion2
+{
+    partial class frmClientes
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgClientes = new System.Windows.Forms.DataGridView();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgClientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgClientes
+            // 
+            this.dgClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgClientes.Location = new System.Drawing.Point(12, 202);
+            this.dgClientes.Name = "dgClientes";
+            this.dgClientes.Size = new System.Drawing.Size(776, 150);
+            this.dgClientes.TabIndex = 0;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(549, 411);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(85, 13);
+            this.lblTotal.TabIndex = 1;
+            this.lblTotal.Text = "Total de clientes";
+            this.lblTotal.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(365, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Listado de Clientes";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // frmClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.dgClientes);
+            this.Name = "frmClientes";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmClientes_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgClientes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgClientes;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ pjConexion2/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// import  SqlClient Data Database
+using System.Data.SqlClient;
+
+namespace pjConexion2
+{
+    public partial class frmClientes : Form
+    {
+        public frmClientes()
+        {
+            InitializeComponent();
+        }
+
+
+        //Methods
+
+        public void ListarClientes()
+        {
+            using (SqlDataAdapter SQLDTAPTR_adapter = new SqlDataAdapter("Select * from Clientes", SQLCON_connection))
+            {
+                using (DataSet DTASET_dataset = new DataSet())
+                {
+                    SQLDTAPTR_adapter.Fill(DTASET_dataset.Tables["Clientes"]);
+                    lblTotal.Text = DTASET_dataset.Tables["Clientes"].Rows.Count.ToString();
+                }
+            }
+        }
+      
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmClientes_Load(object sender, EventArgs e)
+        {
+            ListarClientes();
+        }
+
+        //Conection BD
+        SqlConnection SQLCON_connection = new SqlConnection("Data Source=(local);Initial Catalog=Neptuno;Integrated Security=True");
+
+
+    }
+}
